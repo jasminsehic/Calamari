bad5b0be396026a15e8163f08b0cfd949674f4e0
michaelnoonan <michaelnoonan@gmail.com>
2016-02-16 16:03:54 +1000

    DEBUG: Try timestamp instead of checksum

--- source/Calamari.Azure/Deployment/Conventions/AzureWebAppConvention.cs
+++ source/Calamari.Azure/Deployment/Conventions/AzureWebAppConvention.cs
@@ -91,7 +91,7 @@ namespace Calamari.Azure.Deployment.Conventions
             var syncOptions = new DeploymentSyncOptions
             {
                 WhatIf = false,
-                UseChecksum = true,
+                UseChecksum = false, // DEBUG DEBUG DEBUG!!!
                 DoNotDelete = !variables.GetFlag(SpecialVariables.Action.Azure.RemoveAdditionalFiles),
             };
 
